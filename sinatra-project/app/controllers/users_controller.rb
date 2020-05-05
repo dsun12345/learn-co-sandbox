2b57ed098276f9abf44b013532ec4ce11ac2ea0f
Daniel Sun <danielsun12345@gmail.com>
2020-05-05 21:28:35 +0000

    adding users

---
+++ sinatra-project/app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UserController < ApplicationController
+  
+  def '/users/:id' do 
+    
+  end 
+  
+  get '/users/sign_up' do 
+    
+  end 
+  
+  post '/users/sign_up' do 
+  
+  end 
+
+  get '/logout' do 
+    
+  end 
+  
+  delete '/users/deactivate' do 
+    
+  end 
+  
+  
+end 
