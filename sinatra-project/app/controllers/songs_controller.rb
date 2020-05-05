4f60489018deb4663652a15dd60decc2f5416777
Daniel Sun <danielsun12345@gmail.com>
2020-05-05 04:26:31 +0000

    second  commit

--- sinatra-project/app/controllers/songs_controller.rb
+++ sinatra-project/app/controllers/songs_controller.rb
@@ -14,30 +14,49 @@ class SongsController < ApplicationController
   post '/songs' do 
     @song = Song.new(params)
     if @song.save
-      erb :'songs/show'
+      redirect "/songs/#{@song.id}"
     else 
       erb :'songs/new'
     end 
   end 
-  
-  
+
   get '/songs/:id' do
     @song = Song.find(params[:id])
     erb :'songs/show'
   end 
   
   get '/songs/:id/edit' do 
-  
+    @song = Song.find(params[:id])
+    erb :'songs/edit'
   end 
   
   patch '/songs/:id' do 
-    
+    @song = Song.find(params[:id])
+    @song_params = update_list(params)
+    @song.update(@song_params)
+    redirect to "songs/#{@song.id}"
   end 
   
   delete '/songs/:id/delete' do 
-    
+    @song = Song.find(params[:id])
+    @song.destroy 
+    if @song.errors.messages.length <= 0 
+      erb :"songs/index"
+    else
+      erb :"songs/#{@song.id}"
+    end 
   end 
   
+private
+
+  def update_list(params)
+    {
+      name: params[:name],
+      artist: params[:artist],
+      genre: params[:genre],
+      release_date: params[:release_date]
+    }
+  end 
   
   
 end 
